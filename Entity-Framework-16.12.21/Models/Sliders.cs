8806dd77f2eef9259c0cdd3b5897a83f206b1646
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-16 17:57:12 +0400

    Some changes

--- Entity-Framework-16.12.21/Models/Sliders.cs
+++ Entity-Framework-16.12.21/Models/Sliders.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace Entity_Framework_16._12._21.Models
 {
     public class Sliders
     {
+
+             [Key]
         public int Id { get; set; }
+
+
+
+        [MaxLength(100)]
         public string Image { get; set; }
         public int Time { get; set; }
         public string Title { get; set; }
