cc4289dba1db8a5fe397d4c693a840a18ad98590
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-16 17:51:55 +0400

    Add project files.

---
+++ Entity-Framework-16.12.21/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entity_Framework_16._12._21.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Entity-Framework-16.12.21/Models/News.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_16._12._21.Models
+{
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [MaxLength(100)]
+        public string ImgTitle { get; set; }
+
+        public string Img { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string JournalistName { get; set; }
+    }
+}
